aea673fc82fe607b03ab6fd4d034ac6bf85960e6
sstainbr <sstainbr@umich.edu>
2018-12-06 15:45:28 -0500

    wrote test case code

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -1,9 +1,3 @@
-/* 
- * * testSelectionSort.java 
- * *
- * * To change this template, choose Tools | Template Manager 
- * * and open the template in the editor. 
- * */
 
 import static org.junit.Assert.*;
 import org.junit.Assert;
@@ -35,17 +29,59 @@ public class testSelectionSort {
 		Sortedarr[3] = 9;
 		Sortedarr[4] = 10;
 		
-		/** add tests to check for this unit test **/
+		assert (arr == Sortedarr);
 		}
 	public void testNegative(){
-		/** Test data contains negative values only **/
+		int[] arr = new int[5];
+		arr[0] = -8;
+		arr[1] = -9;
+		arr[2] = -7;
+		arr[3] = -10;
+		arr[4] = -2;
+		
+		int[] Sortedarr = new int[5];
+		Sortedarr[0] = -10;
+		Sortedarr[1] = -9;
+		Sortedarr[2] = -8;
+		Sortedarr[3] = -7;
+		Sortedarr[4] = -2;
+		
+		assert (arr == Sortedarr);
 		}
 	
     public void testMixed(){
-    	/** Test data contains with both positive, negative and zeros **/
+    	int[] arr = new int[5];
+		arr[0] = -8;
+		arr[1] = -9;
+		arr[2] = 7;
+		arr[3] = 10;
+		arr[4] = 0;
+		
+		int[] Sortedarr = new int[5];
+		Sortedarr[0] = -9;
+		Sortedarr[1] = -8;
+		Sortedarr[2] = 0;
+		Sortedarr[3] = 7;
+		Sortedarr[4] = 10;
+		
+		assert (arr == Sortedarr);
     	}
     
     public void testDuplicates(){
-    	/** Test data contains duplicates **/
+    	int[] arr = new int[5];
+		arr[0] = 8;
+		arr[1] = 8;
+		arr[2] = -7;
+		arr[3] = 10;
+		arr[4] = 10;
+		
+		int[] Sortedarr = new int[5];
+		Sortedarr[0] = -7;
+		Sortedarr[1] = 8;
+		Sortedarr[2] = 8;
+		Sortedarr[3] = 10;
+		Sortedarr[4] = 10;
+		
+		assert (arr == Sortedarr);
     	}
     }
